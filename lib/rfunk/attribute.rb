d19eb9dc6cb28ad93ac1cd8453f1fa942bfb971e
Alex Falkowski <alexrfalkowski@gmail.com>
2013-10-05 10:52:11 -0400

    Added the ability to create an object with constructor parameters.

--- lib/rfunk/attribute.rb
+++ lib/rfunk/attribute.rb
@@ -1,15 +1,39 @@
 module RFunk
   module Attribute
+    ATTRIBUTES_VARIABLE_NAME = '@attributes'
+
     def self.included(base)
       base.extend(ClassMethods)
     end
 
+    def initialize(options = {})
+      attributes = self.class.instance_variable_get(ATTRIBUTES_VARIABLE_NAME)
+
+      options.each { |key, value|
+        unless attributes.key?(key)
+          raise "Attribute with name '#{key}' does not exist. The only available attributes are '#{attributes}'"
+        end
+
+        type = attributes[key]
+
+        raise "Expected a type of '#{type}' for attribute '#{key}'" unless value.instance_of?(type)
+
+        variable_name = "@#{key}"
+        self.instance_variable_set(variable_name, value)
+      }
+    end
+
     module ClassMethods
       def attribute(name, type)
+        attributes = self.instance_variable_get(ATTRIBUTES_VARIABLE_NAME) || {}
+        attributes[name] = type
+        self.instance_variable_set(ATTRIBUTES_VARIABLE_NAME, attributes)
+
         variable_name = "@#{name}"
+
         self.send :define_method, name do |value = nil|
           if value
-            raise "Expected a type of '#{type}'" unless value.instance_of?(type)
+            raise "Expected a type of '#{type}' for attribute '#{name}'" unless value.instance_of?(type)
 
             self.class.new.tap { |object|
               self.instance_variables.select { |v| v != variable_name }.each { |v|
