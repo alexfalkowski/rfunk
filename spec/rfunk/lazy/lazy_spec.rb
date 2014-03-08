19a530308d7543e369790f295239cf423bf0b3f2
Alex Falkowski <alexrfalkowski@gmail.com>
2014-03-08 11:04:27 +1100

    Added the ability to check whether a lazy is created.

--- spec/rfunk/lazy/lazy_spec.rb
+++ spec/rfunk/lazy/lazy_spec.rb
@@ -11,6 +11,7 @@ describe RFunk::Lazy do
         When(:result) { lazy.value }
         Then { expect(array).to eq(['one']) }
         Then { expect(result).to eq(Some(['one'])) }
+        Then { expect(lazy.created?).to be_true }
       end
 
       context 'get a value once' do
@@ -18,6 +19,7 @@ describe RFunk::Lazy do
         When(:result) { lazy.value }
         Then { expect(array).to eq(['one']) }
         Then { expect(result).to eq(Some(['one'])) }
+        Then { expect(lazy.created?).to be_true }
       end
     end
 
@@ -25,10 +27,13 @@ describe RFunk::Lazy do
       Given(:lazy) { Lazy(-> { nil }) }
       When(:result) { lazy.value }
       Then { expect(result).to eq(None()) }
+      Then { expect(lazy.created?).to be_true }
     end
 
     context 'does not get a value' do
+      Given(:lazy) { Lazy(-> { array << 'one' }) }
       Then { expect(array).to be_empty }
+      Then { expect(lazy.created?).to be_false }
     end
   end
 end
