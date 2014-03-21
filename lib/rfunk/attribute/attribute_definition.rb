b180bc5560791438bdc27c6fb6900270ac789bc1
Alex Falkowski <alexrfalkowski@gmail.com>
2014-03-21 23:40:34 +1100

    Moved the attribute definition to its own module.

---
+++ lib/rfunk/attribute/attribute_definition.rb
@@ -0,0 +1,21 @@
+module RFunk
+  module AttributeDefinition
+    def attribute(name, type, options = {})
+      AttributeVariable.new.add(instance: self,
+                                name: name,
+                                type: type,
+                                options: options)
+
+      define_method(name) { |value = nil|
+        if value
+          ErrorChecking.new.raise_expected_type(name, value, type)
+          Immutable.new.create(instance: self,
+                               variable_name: variable_name(name),
+                               value: value)
+        else
+          Option(self.instance_variable_get(variable_name(name)))
+        end
+      }
+    end
+  end
+end
