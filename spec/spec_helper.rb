3a8dad65e2f386048c7225b17e7e062b72feb5ee
Alex Falkowski <alexrfalkowski@gmail.com>
2017-03-07 08:36:57 +0000

    Add code coverage to Circle.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,12 @@ $LOAD_PATH.unshift File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib
 
 require 'simplecov'
 
+# save to CircleCI's artifacts directory if we're on CircleCI
+if ENV['CIRCLE_ARTIFACTS']
+  dir = File.join(ENV['CIRCLE_ARTIFACTS'], "coverage")
+  SimpleCov.coverage_dir(dir)
+end
+
 SimpleCov.start do
   add_filter '/spec/'
 end
